a5b74d820411f3a98be680577f0fbac726760a09
Leah-Belin <LazrGoose@gmail.com>
2018-07-29 19:44:58 -0400

    Create intents.js

---
+++ intents.js
@@ -0,0 +1,85 @@
+{
+    "interactionModel": {
+        "languageModel": {
+            "invocationName": "laurel music camp facts",
+            "intents": [
+                {
+                    "name": "AMAZON.HelpIntent",
+                    "samples": [
+                        "help",
+                        "what can I say",
+                        "what can I ask",
+                        "help me"
+                    ]
+                },
+                {
+                    "name": "AMAZON.StopIntent",
+                    "samples": [
+                        "stop",
+                        "end"
+                    ]
+                },
+                {
+                    "name": "GetNewFactIntent",
+                    "slots": [],
+                    "samples": [
+                        "a fact",
+                        "a camp fact",
+                        "a music camp fact",
+                        "a laurel fact",
+                        "a laurel music camp fact",
+                        "tell me a fact",
+                        "tell me a camp fact",
+                        "tell me a music camp fact",
+                        "tell me a laurel fact",
+                        "tell me a laurel music camp fact",
+                        "give me a fact",
+                        "give me a laurel fact",
+                        "give me a camp fact",
+                        "give me a music camp fact",
+                        "give me a laurel music camp fact",
+                        "tell me trivia",
+                        "tell me laurel trivia",
+                        "tell me laurel music camp trivia",
+                        "tell me music camp trivia",
+                        "give me trivia",
+                        "give me camp trivia",
+                        "give me music camp trivia",
+                        "give me laurel trivia",
+                        "give me laurel music camp trivia",
+                        "give me some information",
+                        "give me some laurel information",
+                        "give me some laurel music camp information",
+                        "give me some camp information",
+                        "give me some music camp information",
+                        "give me camp information",
+                        "give me music camp information",
+                        "give me laurel music camp information",
+                        "tell me something",
+                        "give me something"
+                    ]
+                },
+                {
+                    "name": "CountdownIntent",
+                    "slots": [],
+                    "samples": [
+                        "how many days until laurel music camp",
+                        "how many days until laurel",
+                        "how many days until camp",
+                        "how long until laurel music camp",
+                        "how long until laurel",
+                        "how long until camp",
+                        "when is Laurel Music Camp",
+                        "when is laurel",
+                        "when is camp"
+                    ]
+                },
+                {
+                    "name": "AMAZON.FallbackIntent",
+                    "samples": []
+                }
+            ],
+            "types": []
+        }
+    }
+}
